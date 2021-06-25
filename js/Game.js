8627196a04d47e26b12ce3033891cc31d6cea8d6
System Administrator <root@Nazariys-MacBook-Pro.local>
2021-06-25 01:52:59 -0700

    Tic Tac toe Logic

---
+++ js/Game.js
@@ -0,0 +1,69 @@
+export default class Game {
+
+    constructor() {
+        this.newGame();
+    }
+
+    newGame(){
+        this.turn = "X";
+        this.board = new Array(9).fill(null);
+        this.winner = null;
+    }
+
+    nextTurn() {
+        if(this.turn === "X"){
+            this.turn = "O" 
+        } else {
+            this.turn = "X"
+        }
+    }
+
+    makeMove(i) {
+        if(!this.isInProgress()){
+            return;
+        }
+
+        if(this.board[i]){
+            return;
+        }
+        this.board[i] = this.turn;
+        let winningCombination = this.findWinningCombination()
+        if(!winningCombination){
+            this.nextTurn();
+        } else {
+            this.winner = winningCombination[0]
+        }
+        
+    }
+
+    findWinningCombination(){
+        const winningCombinations = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [6,4,2]
+        ]
+
+        for(const combination of winningCombinations){
+            const [a,b,c] = combination;
+            if(this.board[a] &&
+                (this.board[a] === this.board[b] && this.board[a] === this.board[c])){
+                return combination;
+            }
+        }
+        return null;
+    }
+
+    isInProgress(){
+        if(!this.findWinningCombination() && this.board.includes(null)){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
---
+++ js/GameView.js
@@ -0,0 +1,75 @@
+export default class GameView {
+    constructor(root){
+        this.root = root;
+        this.root.innerHTML = `
+            <div class="header">
+                <div class="header-turn"></div>
+                <div class="header-status"></div>
+                <button type="button" class="header-restart">
+                    Restart
+                </button>
+            </div>
+            <div class="board">
+                <div class="board-tile" data-index="0"></div>
+                <div class="board-tile" data-index="1"></div>
+                <div class="board-tile board-tile-winner" data-index="2"></div>
+                <div class="board-tile" data-index="3"></div>
+                <div class="board-tile" data-index="4"></div>
+                <div class="board-tile" data-index="5"></div>
+                <div class="board-tile" data-index="6"></div>
+                <div class="board-tile" data-index="7"></div>
+                <div class="board-tile" data-index="8"></div>
+            </div>
+        `
+
+        this.onTileClick = undefined;
+        this.onRestartClick = undefined;
+
+        this.root.querySelectorAll(".board-tile").forEach(tile=>{
+            tile.addEventListener("click", ()=>{
+                this.onTileClick(tile.dataset.index);
+            })
+        })
+
+        this.root.querySelector(".header-restart").addEventListener("click", ()=>{
+            this.onRestartClick();
+        })
+    }
+
+
+    update(game){
+        this.updateTurn(game);
+        this.updateStatus(game);
+        this.updateBoard(game);
+    }
+
+    updateTurn(game){
+        this.root.querySelector(".header-turn").textContent = `${game.turn}'s turn`
+    }
+
+    updateStatus(game){
+        let status = "In Progress";
+        if(game.findWinningCombination()){
+            status = `${game.turn} is the winner`;
+        } else if (!game.isInProgress()){
+            status = "It's a tie";
+        }
+
+        this.root.querySelector(".header-status").textContent = status;
+    }
+
+    updateBoard(game){
+        const winningCombination = game.findWinningCombination();
+
+        for(let i = 0; i < game.board.length; i++){
+            const tile = this.root.querySelector(`.board-tile[data-index="${i}"]`)
+            tile.classList.remove("board-tile-winner");
+            if(game.board[i]){
+                tile.textContent =  game.board[i];
+            }
+            if(winningCombination && winningCombination.includes(i)){
+                tile.classList.add("board-tile-winner");
+            }
+        }
+    }
+}
